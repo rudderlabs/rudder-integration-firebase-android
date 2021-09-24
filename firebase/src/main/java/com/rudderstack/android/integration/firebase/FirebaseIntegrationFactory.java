1c5b01776a37c0e6b803aba17215051df064b8c5
1abhishekpandey <abhishekpandey10196@gmail.com>
2021-09-24 15:55:24 +0530

    Added support for "revenue", "value","total" and getFloat()

--- firebase/src/main/java/com/rudderstack/android/integration/firebase/FirebaseIntegrationFactory.java
+++ firebase/src/main/java/com/rudderstack/android/integration/firebase/FirebaseIntegrationFactory.java
@@ -30,7 +30,7 @@ public class FirebaseIntegrationFactory extends RudderIntegration<FirebaseAnalyt
 
     private static final List<String> RESERVED_PARAM_NAMES = Arrays.asList(
             "product_id", "name", "category", "quantity", "price", "currency",
-            "value", "order_id", "tax", "shipping", "coupon"
+            "value", "revenue", "total", "order_id", "tax", "shipping", "coupon"
     );
 
     public static Factory FACTORY = new Factory() {
@@ -224,9 +224,12 @@ public class FirebaseIntegrationFactory extends RudderIntegration<FirebaseAnalyt
     private void addOrderProperties(Bundle params, Map<String, Object> properties) {
         if (params != null && properties != null) {
             try {
-                if (properties.containsKey("value")) {
-                    String value = (String) properties.get("value");
-                    params.putFloat(FirebaseAnalytics.Param.VALUE, Float.parseFloat(value != null ? value : "0"));
+                if (properties.containsKey("revenue")) {
+                    params.putFloat(FirebaseAnalytics.Param.VALUE, Utils.getFloat(properties.get("revenue")));
+                } else if (properties.containsKey("value")) {
+                    params.putFloat(FirebaseAnalytics.Param.VALUE, Utils.getFloat(properties.get("value")));
+                } else if (properties.containsKey("total")) {
+                    params.putFloat(FirebaseAnalytics.Param.VALUE, Utils.getFloat(properties.get("total")));
                 }
                 if (properties.containsKey("currency")) {
                     params.putString(FirebaseAnalytics.Param.CURRENCY, (String) properties.get("currency"));
